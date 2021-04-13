e41179282ea0dcd2990a02bfd394a3c3b262b20e
Robin <robiin-98@hotmail.com>
2021-04-13 12:00:44 +0200

    new route to words

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
   res.status(200).json({message: "Hello World"})
 })
 
+app.get('/words', (req, res) => {
+  res.status(200).json({message: "Hello Words"})
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
