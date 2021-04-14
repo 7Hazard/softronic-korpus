b8dc03168d8696fdff97f8fe26167d04c3ea9fa6
Hazard <hazard9909@gmail.com>
2021-04-14 23:25:08 +0200

    added some delete tests

--- backend/__tests__/words.ts
+++ backend/__tests__/words.ts
@@ -13,17 +13,16 @@ beforeAll(async () => {
   db = stuff.db
 });
 
-afterAll((done)=>{
-  server.close(()=>{
+afterAll((done) => {
+  server.close(() => {
     done()
   })
 })
 
 
-it("add", async () => {
-  
+test("add", async () => {
   let resp = await api.post("/words")
-    .send({text: "hello",})
+    .send({ text: "hello" })
     .expect(200)
     .expect({
       text: "hello",
@@ -31,7 +30,7 @@ it("add", async () => {
     })
 });
 
-it("get", async () => {
+test("get", async () => {
   let resp = await api.get("/words")
     .expect(200)
     .expect([{
@@ -40,7 +39,7 @@ it("get", async () => {
     }])
 });
 
-it("get specific", async () => {
+test("get specific", async () => {
   let resp = await api.get("/words/1")
     .expect(200)
     .expect({
@@ -49,27 +48,67 @@ it("get specific", async () => {
     })
 });
 
-it("add duplicate", async () => {
+test("add duplicate", async () => {
   let resp = await api.post("/words")
-    .send({text: "hello"})
+    .send({ text: "hello" })
     .expect(409)
 });
 
-it("add bad words", async () => {
-  await api.post("/words").send({text:"with space"}).expect(400)
-  await api.post("/words").send({text:"345345"}).expect(400)
-  await api.post("/words").send({text:"@£$!?=)(/&[]{}%"}).expect(400)
-  await api.post("/words").send({text:"longtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtext"}).expect(400)
-  await api.post("/words").send({text:""}).expect(400)
+test("add bad words", async () => {
+  await api.post("/words").send({ text: "with space" }).expect(400)
+  await api.post("/words").send({ text: "345345" }).expect(400)
+  await api.post("/words").send({ text: "@£$!?=)(/&[]{}%" }).expect(400)
+  await api.post("/words").send({ text: "longtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtextlongtext" }).expect(400)
+  await api.post("/words").send({ text: "" }).expect(400)
 });
 
-test("Delete existing",async ()=>{
-  await api.delete("/words").send({id:1}).expect(200)
+test("delete one existing", async () => {
+  await api.delete("/words").send({ id: 1 }).expect(200).expect({deletedCount: 1})
 });
 
-test("Delete none existing",async ()=>{
-  await api.delete("/words").send({id:1}).expect(404)
+test("delete multiple", async () => {
+  // add three words
+  let word1 = await addWord("hello")
+  let word2 = await addWord("bye")
+  let word3 = await addWord("goodbye")
+
+  // delete all
+  await api.delete("/words")
+    .send({ ids: [word1, word2, word3] })
+    .expect(200)
+    .expect({ deletedCount: 3 })
+})
+
+test("delete none existing", async () => {
+  await api.delete("/words")
+    .send({ id: 1 })
+    .expect(200)
+    .expect({ deletedCount: 0 })
+});
+
+test("delete with bad input", async () => {
+  await expectErrors(api.delete("/words"), { id: {} }, 400)
+  await expectErrors(api.delete("/words"), { id: [] }, 400)
+  await expectErrors(api.delete("/words"), { id: "" }, 400)
+  await expectErrors(api.delete("/words"), { ids: "" }, 400)
+  await expectErrors(api.delete("/words"), { ids: {} }, 400)
 });
 
 
 
+
+
+/////
+///// Helper functions
+/////
+async function expectErrors(method: request.Test, requestBody: Object, code: number) {
+  let resp = await method.send(requestBody).expect(code)
+  expect(resp.body).toHaveProperty("errors")
+  return resp
+}
+
+// Returns id
+async function addWord(text: string): Promise<number> {
+  let response = await api.post("/words").send({ text: text })
+  return response.body.id
+}
