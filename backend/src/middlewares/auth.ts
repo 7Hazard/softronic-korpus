265486d57308da3fee1b0971dd8ce8c644174f25
Hazard <hazard9909@gmail.com>
2021-04-23 07:02:19 +0200

    Add creation date in JWT data

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -1,33 +1,33 @@
 
 import jwt from "jsonwebtoken";
-import { User, Users } from "../entities/User";
+import { Users } from "../entities/User";
 
-export async function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1] // if we have an authHeader,then return authHeader portion
+export async function authToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1] // if we have an authHeader,then return authHeader portion
 
-  if (token == null)
-      return res.sendStatus(401)
+    if (token == null)
+        return res.sendStatus(401)
 
-  try {
-      var decode = jwt.verify(token, 'shhhhh');
-  } catch (err) {
-      res.sendStatus(401);
-      return;
-  }
+    try {
+        var decode = jwt.verify(token, 'shhhhh');
+    } catch (err) {
+        res.sendStatus(401);
+        return;
+    }
 
-  var user = decode["name"];
-  let userDb = await Users.getOne(user);
+    var username = decode["name"];
+    let user = await Users.getOne(username);
 
-  if (userDb === null) {
-      res.sendStatus(401);
-      return;
-  }
+    if (user === null) {
+        res.sendStatus(401);
+        return;
+    }
 
-  if (token !== userDb.token) { // checks if token exits in database
-      res.sendStatus(401);
-      return;
-  }
+    if (token !== user.token) { // checks if token exits in database
+        res.sendStatus(401);
+        return;
+    }
 
-  next()
+    next()
 }
