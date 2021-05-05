5389fdded40859c848451641176c95d5849ef2f0
Mohamed Osman <Mohosm@kth.se>
2021-05-05 19:33:14 +0200

    Added some extra tests

--- backend/tests/__tests__/synonyms.ts
+++ backend/tests/__tests__/synonyms.ts
@@ -4,6 +4,7 @@ import { addWord, api } from "../helpers";
 let hello = await addWord("hello")
 let hi = await addWord("hi")
 let hey = await addWord("hey")
+let greetings = await addWord("greetings")
 
 
 test("add synonym", async () => {
@@ -15,6 +16,16 @@ test("add synonym", async () => {
     .expect(200, {
       phrase: hello.id, // the phrase to equalize
       meaning: hi.id // the id to the equivalent phrase
+    })
+
+    await api.post("/synonyms").authenticate()
+    .send({
+      phrase: hey.id, // the id of the phrase to equalize       //3 -> 4
+      meaning: greetings.id // the id to the equivalent phrase
+    })
+    .expect(200, {
+      phrase: hey.id, // the phrase to equalize
+      meaning: greetings.id // the id to the equivalent phrase
     })
 
   await api.post("/synonyms").authenticate()
@@ -40,15 +51,35 @@ test("add synonym", async () => {
     .expect(400, { error: "No circular or transitive dependencies allowed" })
 
 })
+test("update synonym", async () => {
+
+  // search by phrase
+  await api.put(`/synonyms/`).authenticate()
+    .send({
+      phraseId: hey.id,
+      newMeaningId: hello.id
+    })
+    .expect(400, { error: "No circular or transitive dependencies allowed" })
+
+    await api.put('/synonyms/').authenticate()
+      .send({
+          phraseId: hey.id,
+          newMeaningId: hi.id
+      })
+      .expect(200, {
+        phraseId: hey.id, // the phrase to equalize
+        newMeaningId: hi.id // the id to the equivalent phrase
+      })
+});
 
 test("get all synonyms", async () => {
   await api.get("/synonyms")
     .expect(200, [
       {
         phrase: { text: 'hello', id: 1 }, // the phrase to equalize
-        meaning: { text: 'hi', id: 2 }, // the id to the equivalent phrase
-
-      }
+        meaning: { text: 'hi', id: 2 } // the id to the equivalent phrase
+      },
+      { phrase: {text: 'hey', id: 3}, meaning: {text: 'hi', id:2}}
     ])
 });
 
@@ -56,10 +87,10 @@ test("get specific synonym", async () => {
   // TODO invalid input tests
 
   // search by phrase
-  await api.get(`/synonyms/${hello.id}`)
+  await api.get(`/synonyms/1`)
     .expect(200, [{ phrase: hello, meaning: hi }])
 
   // search by meaning
-  await api.get(`/synonyms/${hi.id}`)
-    .expect(200, [{ phrase: hello, meaning: hi }])
+  await api.get(`/synonyms/3`)
+    .expect(200, [{ phrase: hey, meaning: hi }])
 });
