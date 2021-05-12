884a625400a639d42d63cbfa283cbc290d732aee
Mill3nium <milan-l97@hotmail.com>
2021-05-12 13:06:44 +0200

    Added Custom Data

--- frontend/src/app/showing-phrases/showing-phrases.component.ts
+++ frontend/src/app/showing-phrases/showing-phrases.component.ts
@@ -8,23 +8,21 @@ import {AfterViewInit, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  phrase: string;
+  id: number;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {id: 1, phrase: 'Läsk'},
+  {id: 2, phrase: 'Dryck'},
+  {id: 3, phrase: 'Cola'},
+  {id: 4, phrase: 'Vätska'},
+  {id: 5, phrase: 'Vin'},
+  {id: 6, phrase: 'Nocco'},
+  {id: 7, phrase: 'Clean'},
+  {id: 8, phrase: 'Monster'},
+  {id: 9, phrase: 'Powerking'},
+  {id: 10, phrase: 'Haiwa'},
 ];
 
 /**
@@ -37,7 +35,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class ShowingPhrasesComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['id', 'phrase', 'synonyms'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
