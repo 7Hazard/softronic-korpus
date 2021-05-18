27b63dff2900c82f04e51adee659c4759cb0537b
Hazard <hazard9909@gmail.com>
2021-05-18 19:33:00 +0200

    Implemented translation based on synonyms

--- backend/src/routes/translations.ts
+++ backend/src/routes/translations.ts
@@ -14,41 +14,48 @@ export default new Routes("/translations").post("/", [], async (req, res) => {
     }
 
     // build phrase-meaning map
-    let phrases = await Phrases.getAllWithRelations(["synonym", "synonym.meaning"])
-    let phraseMeaningMap = new Map<string, string>()
+    let phrases = await Phrases.getAllWithRelations([
+        "synonym",
+        "synonym.meaning",
+    ])
+    let dictionary = new Dictionary()
     for (const phrase of phrases) {
         let synonym = phrase.synonym as Synonym
         if (!synonym) continue
         let meaning = synonym.meaning as Phrase
-        phraseMeaningMap.set(phrase.text, meaning.text)
+        dictionary.set(phrase.text, meaning.text)
     }
 
-    let text = new Text(req.body.text)
-    let result = text.translate(phraseMeaningMap)
+    let text = new Text(req.body.text, dictionary)
+    let result = text.translation
 
     // send translation
     res.status(200).send({ translation: result })
 })
 
+class Dictionary extends Map<string, string> {}
 class Text {
-    tokens: string[]
+    tokens: Token[] = []
     phraseCandidates = new PhraseCandidateSet()
 
-    constructor(text: string) {
+    constructor(text: string, dictionary: Dictionary) {
         // Split text into tokens
-        this.tokens = text.split(" ")
+        let tmp = text.split(" ")
+        tmp.forEach((element, i) => {
+            this.tokens.push(new Token(element, i))
+        });
 
-        let start = 0;
-        let end = 0;
+        let start = 0
+        let end = 0
         // let lastTokenIndex = this.tokens.length-1;
         let tokensCount = this.tokens.length
         while (start != tokensCount && end != tokensCount) {
-            let tokenCandidates: string[] = []
+            let tokenCandidates: Token[] = []
             while (end != tokensCount) {
                 tokenCandidates.push(this.tokens[end])
 
                 // make phrase candidate
-                let phraseCandidate = new PhraseCandidate(tokenCandidates);
+                let phraseCandidate = new PhraseCandidate(tokenCandidates)
                 // add to phrase candidates
                 this.phraseCandidates.add(phraseCandidate)
 
@@ -60,56 +67,95 @@ class Text {
 
         // sort phrase candidates
         this.phraseCandidates.sort()
-    }
+        
+        for (const candidate of this.phraseCandidates) {
+            let str = candidate.toString()
+            let translation = dictionary.get(str)
+            if(!translation) continue
 
-    translate(translations: Map<string, string>) {
+            // position of token in tokens
+            let candidatePosition = candidate.tokens[0].position
+
+            // build tokens from translation
+            let translatedSplit = translation.split(" ")
+            let translatedTokens: Token[] = []
+            for (let i = 0; i < translatedSplit.length; i++) {
+                const element = translatedSplit[i];
+                translatedTokens.push(new Token(element, i+candidatePosition))
+            }
+
+            // remove tokens from current and insert translated tokens
+            this.tokens.splice(candidatePosition, candidate.tokens.length, ...translatedTokens)
+
+            // update new positions after candidatePosition+candidate.tokens.length
+            for (let i = candidatePosition+translatedTokens.length; i < this.tokens.length; i++) {
+                const token = this.tokens[i]
+                token.position -= translatedTokens.length
+            }
+        }
+    }
+    
+    public get translation() {
         return this.tokens.join(" ")
     }
 }
 
 class Token {
-    content: string
-    index: number
     translated = false
+
+    constructor(public content: string, public position: number)
+    {
+
+    }
+
+    toString() {
+        return this.content
+    }
 }
 
 class PhraseCandidate {
-    tokens: string[]
+    tokens: Token[]
 
-    constructor(tokens: string[]) {
+    constructor(tokens: Token[]) {
         this.tokens = [...tokens]
     }
 
     equals(other: PhraseCandidate) {
-        if(this.tokens.length != other.tokens.length)
-            return false;
+        if (this.tokens.length != other.tokens.length) return false
 
-        for(let i = 0; i < this.tokens.length; i++)
-        {
-            if(this.tokens[i] != other.tokens[i])
-                return false;
+        for (let i = 0; i < this.tokens.length; i++) {
+            if (this.tokens[i] != other.tokens[i]) return false
         }
 
-        return true;
+        return true
+    }
+
+    toString() {
+        return this.tokens.join(" ")
     }
 }
 
 class PhraseCandidateSet {
     candidates: PhraseCandidate[] = []
-    constructor() {
-
-    }
+    constructor() {}
 
     add(candidate: PhraseCandidate) {
         // check if candidate already exists
         for (const cand of this.candidates) {
-            if(cand.equals(candidate)) return
+            if (cand.equals(candidate)) return
         }
 
         this.candidates.push(candidate)
     }
 
     sort() {
-        // sort by most tokens (and characters)
+        // TODO more efficient sorting based on token count
+        this.candidates.sort((a, b) => {
+            if (a.tokens.length < b.tokens.length) return 1
+            else if (a.tokens.length > b.tokens.length) return -1
+            return 0
+        })
     }
+
+    [Symbol.iterator]() { return this.candidates.values() }
 }
